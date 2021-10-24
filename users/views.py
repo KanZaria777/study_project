f52de6fe21785178bf14cab4c0b52df0454aa21e
KanZaria777 <kanzaria777@gmail.com>
2021-10-24 16:18:54 +0300

    Wrote controllers for the Users application and fixed all links as expected

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+# Функции которые тут написаны господь провозгласил контроллерами
 
-# Create your views here.
+
+def login(request):
+    context = {'title': 'GeekShop - Авторизация'}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {'title': 'GeekShop - Регистрация'}
+    return render(request, 'users/registration.html', context)
